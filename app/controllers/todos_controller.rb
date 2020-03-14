e33f3c93eb6399b1a6f674094f899902b13a02ee
aakashananth <aakashananth@gmail.com>
2020-03-14 22:35:44 +0530

    Added todo.rb

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   def index
-    render plain: "Hello, this is /todos! #{DateTime.now.to_s}"
+    render plain: Todo.all.to_a
   end
 end
